871f23cd6458ae0dc9165ddbe3c319e0fb9bcace
anniArrti <anniat44@gmail.com>
2018-06-28 17:06:21 +0530

    make it better

--- app.js
+++ app.js
@@ -22,14 +22,18 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })) 
    
-  
-app.use(function (req, res, next) {        
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});  
+/*app.use(function (req, res, next) {        
      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');    
      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');    
      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');      
      res.setHeader('Access-Control-Allow-Credentials', true);       
      next();  
- });  
+ });*/  
   
 app.use(express.static(__dirname + '/dist'));
 
